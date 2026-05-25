27f7d758aac899e35dec66161c904f40d693ba4a
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-05-25 14:16:19 +0500

    dcvrbf

--- vite-project/src/Companents/Header/Header.jsx
+++ vite-project/src/Companents/Header/Header.jsx
@@ -1,38 +1,43 @@
 import React from 'react';
 // import { useTranslation } from 'react-i18next'; 
-import {useTranslation} from 'react'
+import { useTranslation } from 'react'
 import "./Header.css";
 
 const Header = () => {
-  const { t,i18n } = useTranslation(); 
+    //   const { t,i18n } = useTranslation(); 
 
-  const handleChangeLanguage =(event)=>{
-    i18n.changeLanguage(event.target.value)
-  }
+    const handleChangeLanguage = (event) => {
+        i18n.changeLanguage(event.target.value)
+    }
 
-  return (
-    <header>
-      <div className="container">
-        <div className="navbar">
-          <h1>Education</h1>
+    return (
+        <header>
+            <div className="container">
+                <div className="navbar">
+                    <h1>Education</h1>
 
-          <ul>
-            <li><a href="#">{t('header.home')}</a></li>
-            <li><a href="#">{t('header.courses')}</a></li>
-            <li><a href="#">{t('header.page')}</a></li>
-            <li><a href="#">{t('header.blog')}</a></li>
-            <li><a href="#">{t('header.contact')}</a></li>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Courses</a></li>
+                        <li><a href="#">Page</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Contact</a></li>
 
-              <select name="" id="" onChange={handleChangeLanguage}>
-                <option value="uz">Uzbek</option>
-                <option value="ru">Russ</option>
-                <option value="en">Eng</option>
-              </select>
-          </ul>
-        </div>
-      </div>
-    </header>
-  );
+                        <select name="" id="" onChange={handleChangeLanguage}>
+                            <option value="uz">Uzbek</option>
+                            <option value="ru">Russ</option>
+                            <option value="en">Eng</option>
+                        </select>
+                    </ul>
+
+                    <div className="btn">
+                        <button>Singin</button>
+                        <button className='bn'>Sing Up</button>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
