8d53674cf6963a784abaac40307935f7762d2f52
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-05-25 14:21:46 +0500

    dcvrbf

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./App.css"; 
 import Header from './Companents/Header/Header';
+import Section from "./Companents/Main/Section/Section"
 const App = () => {
   return (
     <>
       <Header></Header>
+      <Section></Section>
     </>
   );
 };
---
+++ vite-project/src/Companents/Main/Section/Section.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Section = () => {
+    return (
+        <>
+            <section>
+                <div className="container">
+                    <div className="section_title">
+                        <h1>Self-Paced Learning Courses Online</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing 
+                            elit. Amet, vestibulum euismod nullam elementum. Urna, 
+                            posuere nisi sit gravida massa. Ultrices bibendum sagittis pellentesque vitae sit.</p>
+                            <button>Start a coures</button>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Section
