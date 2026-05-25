ddb928da175b1777ce1ecf656e3e6d486ed93128
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-05-25 13:54:51 +0500

    dcvrbf

--- vite-project/src/Companents/Header/Header.jsx
+++ vite-project/src/Companents/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next'; // Faqat shu yerda qoldi, 'react' ichidan o'chirildi
+// import { useTranslation } from 'react-i18next'; 
+import {useTranslation} from 'react'
 import "./Header.css";
 
 const Header = () => {
-  const { t,i18n } = useTranslation(); // Tillarni o'qish uchun hookni ishga tushiramiz
+  const { t,i18n } = useTranslation(); 
+
+  const handleChangeLanguage =(event)=>{
+    i18n.changeLanguage(event.target.value)
+  }
 
   return (
     <header>
@@ -12,20 +17,17 @@ const Header = () => {
           <h1>Education</h1>
 
           <ul>
-            {/* JSON fayldagi kalit so'zlarga moslab t() funksiyasi yozildi */}
             <li><a href="#">{t('header.home')}</a></li>
             <li><a href="#">{t('header.courses')}</a></li>
             <li><a href="#">{t('header.page')}</a></li>
             <li><a href="#">{t('header.blog')}</a></li>
             <li><a href="#">{t('header.contact')}</a></li>
 
-            <li>
-              <select name="lang" id="lang-select">
+              <select name="" id="" onChange={handleChangeLanguage}>
                 <option value="uz">Uzbek</option>
                 <option value="ru">Russ</option>
                 <option value="en">Eng</option>
               </select>
-            </li>
           </ul>
         </div>
       </div>
