194b2f263b193b91d7a05f1b4b4eb60b29fc87d4
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-05-29 23:40:59 +0500

    dcvrbf

--- vite-project/src/Companents/Main/Section4/Section4.jsx
+++ vite-project/src/Companents/Main/Section4/Section4.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import './Section4.css'
+import onli from '/src/img/online-learning1.png'
+import note from '/src/img/notes1.png'
+import choi from '/src/img/choices1.png'
+import tech from '/src/img/technical-support1.png'
 
 const Section4 = () => {
   return (
     <>
-    
+    <Section4>
+      <div className="container">
+        <h1>How It Work</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et hendrerit euismod fusce sit. </p>
+        <div className="cards">
+          <div className="card">
+           <img src={onli} alt="Online Courses" />
+           <h3>Learn More Online</h3>
+           <p>Lorem ipsum dolor sit amet, consecte tur adipiscing elit. Aliquam et hendrer it eui smod fusce sit.</p>
+          </div>
+
+          <div className="card">
+           <img src={note} alt="Online Courses" />
+           <h3>Writing Notes</h3>
+           <p>Lorem ipsum dolor sit amet, consecte tur adipiscing elit. Aliquam et hendrer it eui smod fusce sit.</p>
+          </div>
+
+          <div className="card">
+           <img src={choi} alt="Online Courses" />
+           <h3>Interactive Sessions</h3>
+           <p>Lorem ipsum dolor sit amet, consecte tur adipiscing elit. Aliquam et hendrer it eui smod fusce sit.</p>
+          </div>
+
+          <div className="card">
+           <img src={tech} alt="Online Courses" />
+           <h3>Life Time Support</h3>
+           <p>Lorem ipsum dolor sit amet, consecte tur adipiscing elit. Aliquam et hendrer it eui smod fusce sit.</p>
+          </div>
+        </div>
+      </div>
+    </Section4>
     </>
   )
 }
