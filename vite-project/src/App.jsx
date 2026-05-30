77bbb08209f0e0a6ecf73920f3483c38f4dc3f73
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-05-30 13:41:51 +0500

    dxfrcgtc

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,6 +5,7 @@ import Section from './Companents/Main/Section/Section'
 import Section2 from './Companents/Main/Section2/Section2'
 import Section3 from './Companents/Main/Section3/Section3'
 import Section4 from './Companents/Main/Section4/Section4'
+import Section5 from './Companents/Main/Section5/Section5'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
     <Section2></Section2>
     <Section3></Section3>
     <Section4></Section4>
+    <Section5></Section5>
     </>
   )
 }
