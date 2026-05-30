d954436ebac056eec27880531774a148d81be90b
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-05-30 13:01:26 +0500

    dxfrcgtc

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import "./App.css"; 
-import Header from './Companents/Header/Header';
+import React from 'react'
+import './App.css'
+import Header from './Companents/Header/Header'
 import Section from './Companents/Main/Section/Section'
 import Section2 from './Companents/Main/Section2/Section2'
-import Section3 from './Companents/Main/Section3/Section3';
-import Section4 from './Companents/Main/Section4/Section4';
+import Section3 from './Companents/Main/Section3/Section3'
+import Section4 from './Companents/Main/Section4/Section4'
+
 const App = () => {
   return (
     <>
-      <Header></Header>
-      <Section></Section>
-      <Section2></Section2>
-      <Section3></Section3>
-      <Section4></Section4>
+    <Header></Header>
+    <Section></Section>
+    <Section2></Section2>
+    <Section3></Section3>
+    <Section4></Section4>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- vite-project/src/Companents/Main/Section4/Section4.jsx
+++ vite-project/src/Companents/Main/Section4/Section4.jsx
@@ -8,37 +8,38 @@ import tech from '/src/img/technical-support1.png'
 const Section4 = () => {
   return (
     <>
-    <Section4>
-      <div className="container">
-        <h1>How It Work</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et hendrerit euismod fusce sit. </p>
-        <div className="cards">
-          <div className="card">
-           <img src={onli} alt="Online Courses" />
-           <h3>Learn More Online</h3>
-           <p>Lorem ipsum dolor sit amet, consecte tur adipiscing elit. Aliquam et hendrer it eui smod fusce sit.</p>
-          </div>
+      {/* 1. Katta S harfli teg kichik harfli standart <section> ga o'zgartirildi */}
+      <section className="section4">
+        <div className="container">
+          <h1>How It Work</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et hendrerit euismod fusce sit. </p>
+          <div className="cards">
+            <div className="card">
+             <img src={onli} alt="Online Courses" />
+             <h3>Learn More Online</h3>
+             <p>Lorem ipsum dolor sit amet, consecte tur adipiscing elit. Aliquam et hendrer it eui smod fusce sit.</p>
+            </div>
 
-          <div className="card">
-           <img src={note} alt="Online Courses" />
-           <h3>Writing Notes</h3>
-           <p>Lorem ipsum dolor sit amet, consecte tur adipiscing elit. Aliquam et hendrer it eui smod fusce sit.</p>
-          </div>
+            <div className="card">
+             <img src={note} alt="Online Courses" />
+             <h3>Writing Notes</h3>
+             <p>Lorem ipsum dolor sit amet, consecte tur adipiscing elit. Aliquam et hendrer it eui smod fusce sit.</p>
+            </div>
 
-          <div className="card">
-           <img src={choi} alt="Online Courses" />
-           <h3>Interactive Sessions</h3>
-           <p>Lorem ipsum dolor sit amet, consecte tur adipiscing elit. Aliquam et hendrer it eui smod fusce sit.</p>
-          </div>
+            <div className="card">
+             <img src={choi} alt="Online Courses" />
+             <h3>Interactive Sessions</h3>
+             <p>Lorem ipsum dolor sit amet, consecte tur adipiscing elit. Aliquam et hendrer it eui smod fusce sit.</p>
+            </div>
 
-          <div className="card">
-           <img src={tech} alt="Online Courses" />
-           <h3>Life Time Support</h3>
-           <p>Lorem ipsum dolor sit amet, consecte tur adipiscing elit. Aliquam et hendrer it eui smod fusce sit.</p>
+            <div className="card">
+             <img src={tech} alt="Online Courses" />
+             <h3>Life Time Support</h3>
+             <p>Lorem ipsum dolor sit amet, consecte tur adipiscing elit. Aliquam et hendrer it eui smod fusce sit.</p>
+            </div>
           </div>
         </div>
-      </div>
-    </Section4>
+      </section> {/* 2. Bu yer ham yopildi */}
     </>
   )
 }
