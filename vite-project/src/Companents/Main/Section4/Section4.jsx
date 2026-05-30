8449878467502087b315a1cac8015d3eedbdc03c
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-05-30 13:31:29 +0500

    dxfrcgtc

--- vite-project/src/Companents/Main/Section4/Section4.jsx
+++ vite-project/src/Companents/Main/Section4/Section4.jsx
@@ -8,38 +8,43 @@ import tech from '/src/img/technical-support1.png'
 const Section4 = () => {
   return (
     <>
-      {/* 1. Katta S harfli teg kichik harfli standart <section> ga o'zgartirildi */}
       <section className="section4">
         <div className="container">
           <h1>How It Work</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam et hendrerit euismod fusce sit. </p>
+
           <div className="cards">
+
             <div className="card">
-             <img src={onli} alt="Online Courses" />
-             <h3>Learn More Online</h3>
-             <p>Lorem ipsum dolor sit amet, consecte tur adipiscing elit. Aliquam et hendrer it eui smod fusce sit.</p>
+              <img src={onli} alt="rasm"/>
+              <h3>Learn More Online</h3>
+              <a href="#">Lorem ipsum dolor sit amet, consecte tur adipiscing elit. 
+                Aliquam et hendrer it eui smod fusce sit.</a>
             </div>
 
             <div className="card">
-             <img src={note} alt="Online Courses" />
-             <h3>Writing Notes</h3>
-             <p>Lorem ipsum dolor sit amet, consecte tur adipiscing elit. Aliquam et hendrer it eui smod fusce sit.</p>
+              <img src={note} alt="rasm"/>
+              <h3>Writing Notes</h3>
+              <a href="#">Lorem ipsum dolor sit amet, consecte tur adipiscing elit. 
+                Aliquam et hendrer it eui smod fusce sit.</a>
             </div>
 
             <div className="card">
-             <img src={choi} alt="Online Courses" />
-             <h3>Interactive Sessions</h3>
-             <p>Lorem ipsum dolor sit amet, consecte tur adipiscing elit. Aliquam et hendrer it eui smod fusce sit.</p>
+              <img src={choi} alt="rasm"/>
+              <h3>Interactive Sessions</h3>
+              <a href="#">Lorem ipsum dolor sit amet, consecte tur adipiscing elit. 
+                Aliquam et hendrer it eui smod fusce sit.</a>
             </div>
 
             <div className="card">
-             <img src={tech} alt="Online Courses" />
-             <h3>Life Time Support</h3>
-             <p>Lorem ipsum dolor sit amet, consecte tur adipiscing elit. Aliquam et hendrer it eui smod fusce sit.</p>
+              <img src={tech} alt="rasm"/>
+              <h3>Life Time Support</h3>
+              <a href="#">Lorem ipsum dolor sit amet, consecte tur adipiscing elit. 
+                Aliquam et hendrer it eui smod fusce sit.</a>
             </div>
           </div>
         </div>
-      </section> {/* 2. Bu yer ham yopildi */}
+      </section>
     </>
   )
 }
